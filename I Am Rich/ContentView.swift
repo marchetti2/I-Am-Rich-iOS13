ee37c45ab80cd157095562951f494a5a7d3eca13
Mario Alves <marchetti2@gmail.com>
2023-01-16 00:06:03 -0300

    Initial commit

--- I Am Rich/ContentView.swift
+++ I Am Rich/ContentView.swift
@@ -14,11 +14,11 @@ struct ContentView: View {
             Color(.systemTeal)
                 .edgesIgnoringSafeArea(.all)
             VStack {
-                Text("I Am Rich")
+                 Text("I Am Rich")
                     .font(.system(size: 40))
                     .fontWeight(.bold)
                     .foregroundColor(Color.white)
-                Image("diamond")
+                 Image("diamond")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     .frame(width: 200, height: 200, alignment: .center)
